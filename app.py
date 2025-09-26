d049a727e05bce18efc93302cd74543fde448f81
Ferran73 <ferran.sala.sudria@gmail.com>
2025-09-26 10:29:44 +0200

    Add simple Hola mundo application

---
+++ app.py
@@ -0,0 +1,9 @@
+"""Simple application that prints a greeting."""
+
+def main() -> None:
+    """Display a Hola mundo greeting."""
+    print("Hola mundo")
+
+
+if __name__ == "__main__":
+    main()
